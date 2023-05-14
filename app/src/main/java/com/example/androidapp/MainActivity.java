48de4b1cab334323f39caf7db5424e908dbd0b92
JulianChu-1 <1664553649@qq.com>
2023-05-14 18:45:13 +0800

    modified:   app/src/main/java/com/example/androidapp/MainActivity.java

--- app/src/main/java/com/example/androidapp/MainActivity.java
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -97,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
         RequestBody body = RequestBody.create(jsonBody.toString(),JSON);
         Request request = new Request.Builder()
                 .url("https://api.openai.com/v1/completions")
-                .header("Authorization","Bearer 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")
+                .header("Authorization","Bearer api-key")
                 .post(body)
                 .build();
 
